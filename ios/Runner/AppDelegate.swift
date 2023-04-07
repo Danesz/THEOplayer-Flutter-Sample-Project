01166e185b55e9a4ac5e314c1b0b7ea9e6c4db1f
Daniel Dallos <daniel.dallos@theoplayer.com>
2023-04-07 11:33:16 +0200

    added THEOplayer via cocoapods

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import THEOplayerSDK
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    print("THEOplayer", THEOplayer.version)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
